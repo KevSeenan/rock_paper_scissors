0996fc0f3aceedac4965bde362877aa7ae121d5b
KevSeenan <kev.seenan@gmail.com>
2019-08-05 17:18:26 +0100

    Adds tests for hand1 wins or hand2 wins

--- models/rock_paper_scissors.rb
+++ models/rock_paper_scissors.rb
@@ -9,16 +9,16 @@ class Game
     case
       when hand1 == "rock" && hand2 == "scissors"
         return "rock"
-      # when hand1 == "rock" && hand2 == "paper"
-      #   return "paper"
-      # when hand1 == "paper" && hand2 == "scissors"
-      #   return "scissors"
-      # when hand1 == "paper" && hand2 == "rock"
-      #   return "paper"
-      # when hand1 == "scissors" && hand2 == "paper"
-      #   return "scissors"
-      # when hand1 == "scissors" && hand2 == "rock"
-      #   return "rock"
+      when hand1 == "rock" && hand2 == "paper"
+        return "paper"
+      when hand1 == "paper" && hand2 == "scissors"
+        return "scissors"
+      when hand1 == "paper" && hand2 == "rock"
+        return "paper"
+      when hand1 == "scissors" && hand2 == "paper"
+        return "scissors"
+      when hand1 == "scissors" && hand2 == "rock"
+        return "rock"
       end
     end
 
--- specs/rock_paper_scissors_spec.rb
+++ specs/rock_paper_scissors_spec.rb
@@ -3,10 +3,12 @@ require_relative '../models/rock_paper_scissors'
 
 class TestGame < MiniTest::Test
 
-
-
-  def test_play_rock()
+  def test_play_hand1_wins()
     assert_equal("rock", Game.play("rock", "scissors"))
+  end
+
+  def test_play_hand2_wins()
+    assert_equal("scissors", Game.play("scissors", "paper"))
   end
 
 end
