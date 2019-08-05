bf7ed2b9f43cdb72b0ca43edef563600e46c3193
KevSeenan <kev.seenan@gmail.com>
2019-08-05 19:18:57 +0100

    Adds welcome erb and link to results page

--- controller.rb
+++ controller.rb
@@ -4,6 +4,10 @@ require('sinatra/contrib/all') if development?
 require_relative('./models/rock_paper_scissors.rb')
 also_reload('./models/*')
 
+get '/' do
+  erb(:welcome)
+end
+
 get '/play/:hand1/:hand2' do
   game = Game.play(params[:hand1], params[:hand2])
   @result = game.capitalize()
