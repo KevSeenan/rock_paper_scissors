ef98701f492f1120e905d255c0bf722f957353a2
KevSeenan <kev.seenan@gmail.com>
2019-08-05 17:32:19 +0100

    Adds tests return string for winner

--- controller.rb
+++ controller.rb
@@ -1,14 +1,10 @@
-# require('sinatra')
-# require('sinatra/contrib/all') if development?
-#
-# require_relative('./models/rock_paper_scissors.rb')
-# also_reload('./models/*')
-#
-# get '/game' do
-# return "RPS game"
-# end
-#
-# get '/play/:hand1/:hand2' do
-#   game = Game.new(params[:hand1], params[:hand2])
-#   return "Rock Wins!!"
-# end
+require('sinatra')
+require('sinatra/contrib/all') if development?
+
+require_relative('./models/rock_paper_scissors.rb')
+also_reload('./models/*')
+
+get '/play/:hand1/:hand2' do
+  game = Game.new(params[:hand1], params[:hand2])
+    return "Rock Wins!!"
+end
